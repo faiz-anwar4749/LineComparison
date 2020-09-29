6637c648a52251c9f13c79c4130d9d0f4a584663
Faiz <faiz.anwar4749@gmail.com>
2020-09-29 15:04:56 +0530

    [Faiz] Refactor : Committing CompareTo Program

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -7,7 +7,7 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem!");
-            double x1 = 0, x2 = 0, x3=0, x4= 0, y1 = 0, y2 = 0, y3= 0, y4= 0;
+            double x1 = 0, x2 = 0, x3 = 0, x4 = 0, y1 = 0, y2 = 0, y3 = 0, y4 = 0;
             Console.Write("Enter the x1 co-ordinate: ");
             x1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter the y1 co-ordinate: ");
@@ -27,15 +27,19 @@ namespace LineComparisonProblem
             Console.WriteLine($"The four points on the Cartesian Plane are A({x1},{y1}), B({x2},{y2}), C({x3},{y3}), D({x4},{y4})");
             double LengthAB = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             double LengthCD = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            Boolean b = LengthAB.Equals(LengthCD);
-            if(b)
+            int compare = LengthAB.CompareTo(LengthCD);
+            if (compare > 0)
             {
-                Console.WriteLine("Both lines are equal");
+                Console.WriteLine("Length of LineAB is greater than LineCD");
+            }
+            else if (compare < 0)
+            {
+                Console.WriteLine("Length of LineCD is greater than LineAB");
             }
             else
             {
-                Console.WriteLine("Lines are not equal");
-            }   
+                Console.WriteLine("Length of LineCD is equal to LineAB");
+            }
         }
     }
 }
